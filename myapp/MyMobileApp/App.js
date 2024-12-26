8a6dc04c38448d55e2b19a086b8ca6d5524da4d4
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-12-26 15:12:38 +0000

    Created App.js

--- myapp/MyMobileApp/App.js
+++ myapp/MyMobileApp/App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from "react-native-paper";
+import CurrencyConverter from "./components/CurrencyConverter";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <CurrencyConverter></CurrencyConverter>
+        </View>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
