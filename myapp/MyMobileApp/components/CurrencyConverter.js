387351ab25a42b8d90262f49682853dcc597ef01
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-12-26 15:16:36 +0000

    Conversion handler

--- myapp/MyMobileApp/components/CurrencyConverter.js
+++ myapp/MyMobileApp/components/CurrencyConverter.js
@@ -10,3 +10,19 @@ export default function CurrencyConverter() {
         USD: 1,
         LKR: 294.5,
 };
+
+const handleConvert = () => {
+    const numericAmount = parseFloat(amount);
+
+    if (isNaN(numericAmount) || numericAmount <= 0) {
+      Alert.alert("Invalid Input", "Please enter a valid amount.");
+      return;
+    }
+    if (currencyType === "USDtoLKR") {
+      const converted = numericAmount * exchangeRates.LKR;
+      setConvertedAmount(converted.toFixed(2));
+    } else if (currencyType === "LKRtoUSD") {
+      const converted = numericAmount / exchangeRates.LKR;
+      setConvertedAmount(converted.toFixed(2));
+    }
+  };
